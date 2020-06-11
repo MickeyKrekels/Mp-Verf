1bdc0bf8a61676c0bb53436b7aedb740e5d92586
Mickey Krekels <mickeykrekels@hotmail.nl>
2020-06-11 22:11:13 +0200

    Added comment rating in the view

--- Mp-WebApp/Controllers/CommentController.cs
+++ Mp-WebApp/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business_Logic.Models;
+using Business_Logic.Processor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,19 +15,49 @@ namespace Mp_WebApp.Controllers
         {
             return View();
         }
-        public ActionResult Add()
+        [HttpPost]
+        [Authorize(Roles = "Customer")]
+        public ActionResult PostComment(Guid storeItemId, string text, int rating = 0)
         {
-            return View();
-        }
-        public ActionResult Remove()
-        {
-            return View();
-        }
-        public ActionResult Update()
-        {
-            return View();
+            if (text == null || text == "")
+                return RedirectToAction("Details", "Products", new { id = storeItemId });
+
+            string identity = User.Identity.Name;
+            Guid userId = Guid.Parse(identity);
+
+            CommentProcessor.AddComment(userId, storeItemId, text, rating);
+
+            return RedirectToAction("Details", "Products", new { id = storeItemId });
         }
 
-        //reported 
+        [Authorize(Roles = "Customer")]
+        public ActionResult UpdateCommentRating(Guid storeItemId, Guid commentId, int rating = 0)
+        {
+            var model = CommentProcessor.GetUserComment(commentId);
+
+            string identity = User.Identity.Name;
+            Guid userId = Guid.Parse(identity);
+
+            if (model == null || userId != model.OwnerId || rating == 0)
+                return RedirectToAction("Details", "Products", new { id = storeItemId });
+
+            CommentProcessor.UpdateRating(commentId, rating);
+
+            return RedirectToAction("Details", "Products", new { id = storeItemId });
+        }
+
+        [Authorize(Roles = "Customer")]
+        public ActionResult RemoveComment(CommentModel model)
+        {
+            string identity = User.Identity.Name;
+            Guid userId = Guid.Parse(identity);
+
+            if (model == null || userId != model.OwnerId)
+                return RedirectToAction("Details","Products");
+
+            CommentProcessor.RemoveComment(model.Id);
+
+            return RedirectToAction("Details", "Products");
+        }
     }
 }
--- Mp-WebApp/Controllers/ProductsController.cs
+++ Mp-WebApp/Controllers/ProductsController.cs
@@ -125,49 +125,6 @@ namespace Mp_WebApp.Controllers
 
             return View(model);
         }
-        [HttpPost]
-        [Authorize(Roles = "Customer")]
-        public ActionResult PostComment(Guid storeItemId, string text, int rating = 0)
-        {
-            if (text == null || text == "")
-                return RedirectToAction("Details", new { id = storeItemId });
 
-            string identity = User.Identity.Name;
-            Guid userId = Guid.Parse(identity);
-
-            CommentProcessor.AddComment(userId, storeItemId, text, rating);
-
-            return RedirectToAction("Details", new { id = storeItemId });
-        }
-        [HttpPost]
-        [Authorize(Roles = "Customer")]
-        public ActionResult UpdateCommentRating(Guid storeItemId, Guid commentId, int rating = 0)
-        {
-            var model = CommentProcessor.GetUserComment(commentId);
-
-            string identity = User.Identity.Name;
-            Guid userId = Guid.Parse(identity);
-
-            if (model == null||userId != model.OwnerId || rating == 0)
-                return RedirectToAction("Details", new { id = storeItemId });
-
-            CommentProcessor.UpdateRating(commentId, rating);
-
-            return RedirectToAction("Details", new { id = storeItemId});
-        }
-
-        [Authorize(Roles = "Customer")]
-        public ActionResult RemoveComment(CommentModel model)
-        {
-            string identity = User.Identity.Name;
-            Guid userId = Guid.Parse(identity); 
-
-            if (model == null || userId != model.OwnerId)
-                return RedirectToAction("Details");
-
-            CommentProcessor.RemoveComment(model.Id);
-
-            return RedirectToAction("Details");
-        }
     }
 }
